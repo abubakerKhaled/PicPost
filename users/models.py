201b66cb97028fcc5e93bef38e2bc12e82f05ba4
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-11-13 13:25:50 +0200

    add the custom user model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
