7c9f79fc8422a1e9139ab4dd82834e42b0c0fd82
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-12-05 21:47:01 +0200

    adding the basic image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from users.models import CustomUser
+from django.utils.text import slugify
 
-# Create your models here.
+class Image(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='images_created',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title if len(self.title) <= 50 else f'{self.title[:50]} ...'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
