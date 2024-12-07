74efcacab86d1d7eabca03aa4bad38ece5f63b77
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-12-07 21:29:02 +0200

    build the detail url of the image

--- images/urls.py
+++ images/urls.py
@@ -5,4 +5,9 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create_image'),
+    path(
+        'detail/<int:id>/<slug:slug>/',
+        views.image_detail,
+        name='detail'
+    ),
 ]
